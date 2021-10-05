83415043087d45052586e6448b74b62fb8edc14e
Ting-Kim <59888684+Ting-Kim@users.noreply.github.com>
2021-10-05 20:15:03 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/opt/be-recommendation_flask
+cd $REPOSITORY
+
+APP_NAME=be-recommendation_flask
+# JAR_PATH=$REPOSITORY/
+ENV_PATH=/opt/.env
+
+CURRENT_PID=$(pgrep -fl $REPOSITORY/ | grep python | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "ENV PATH 인식 > $ENV_PATH"
+
+nohup python3 -u app.py &
+
+sudo chmod 777 $REPOSITORY/nohup.out
